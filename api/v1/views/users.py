2a2e7d61403b00a69ebe80803ce5aea12563686c
AdrianaAriza <1296@holbertonschool.com>
2020-04-29 16:28:45 -0500

    [Upload]: Task10

---
+++ api/v1/views/users.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+""""""
+from api.v1.views import app_views
+from models.state import State
+from models import storage
+from flask import jsonify, abort, request
+
+from models.user import User
+
+
+@app_views.route('/users', strict_slashes=False, methods=['GET'])
+def states_all():
+    """"""
+    users = []
+    for v in storage.all(User).values():
+        users.append(v.to_dict())
+    return jsonify(users)
+
+
+@app_views.route('/users/<id>', strict_slashes=False,
+                 methods=['GET', 'DELETE', 'PUT'])
+def state_by_id(user_id):
+    """"""
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    if request.method == 'GET':
+        return jsonify(user.to_dict())
+    if request.method == 'DELETE':
+        user.delete()
+        storage.save()
+        return jsonify({}), 200
+    if request.method == 'PUT':
+        if not request.json:
+            abort(400, "Not a JSON")
+        put = request.get_json()
+        for k, v in put.items():
+            if k == "name":
+                setattr(user, k, v)
+        user.save()
+        return jsonify(user.to_dict()), 200
+
+
+@app_views.route('/states', strict_slashes=False, methods=['POST'])
+def state_new():
+    """New State"""
+    if not request.json:
+        abort(400, "Not a JSON")
+    post = request.get_json()
+    if "name" in post:
+        n_state = State(**post)
+        n_state.save()
+        return jsonify(n_state.to_dict()), 201
+    else:
+        abort(400, "Missing name")
