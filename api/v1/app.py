dfba4d7b85999a86f071c11a4cdd7a9e4a2e7c1c
AdrianaAriza <1296@holbertonschool.com>
2020-04-29 00:01:55 -0500

    [Fix]: app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-from flask import Flask, jsonify, request
+""""""
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
@@ -15,7 +17,7 @@ def teardown_db(exception):
 
 @app.errorhandler(404)
 def _handle_api_error(ex):
-    """"""
+    """api error"""
     return jsonify({"error": "Not found"}), 404
 
 
