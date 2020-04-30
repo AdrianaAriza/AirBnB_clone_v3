6647e8755edc339bb1c81a53ed8acb4d8f6158dd
campopinillos <1207@holbertonschool.com>
2020-04-30 09:39:52 -0500

    [Fix] Hexdigest

--- models/user.py
+++ models/user.py
@@ -28,6 +28,6 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         if 'password' in kwargs:
-            self.password = hashlib.md5(kwargs['password'].
-                                        encode()).hexdigest()
+            pwd = hashlib.md5(kwargs['password'].encode())
+            self.password = pwd.hexdigest()
         super().__init__(*args, **kwargs)
